336faa30470da2468ad79b34c4f7b84ab77b1738
jbDelafosse <jean-baptiste@lumapps.com>
2018-06-13 23:00:55 +0200

    feat(trello-event): add route to receive webhook events

--- app/trello_task.py
+++ app/trello_task.py
@@ -2,5 +2,9 @@ from . import app
 from flask import request
 
 @app.route('/', methods=['GET'])
-def handle_trello_event():
+def hello_world():
     return 'Hello, World!'
+
+@app.route('/trello/event', methods=['GET'])
+def handle_trello_request():
+    return 'Handle trello request here'
