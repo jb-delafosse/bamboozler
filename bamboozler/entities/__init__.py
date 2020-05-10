5adf2c9e347d87566eded8cc766f4b886b0aec6c
jbDelafosse <jean-baptiste@lumapps.com>
2020-05-10 15:39:19 +0200

    feat: add Reminder DTO

---
+++ bamboozler/entities/__init__.py
@@ -0,0 +1,6 @@
+"""This module contains the purest business classes.
+It correspond to highest level of the clean architecture.
+In practice, this means that this module can't import any other
+modules.
+But it's still possible to import other entities.
+"""
---
+++ bamboozler/entities/reminder.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
+
+class ReminderStatus(Enum):
+    TODO = "TODO"
+    DONE = "DONE"
+
+
+@dataclass(frozen=True)
+class Reminder:
+    # pylint: disable=too-many-instance-attributes
+    reminder_id: str
+    title: str
+    notes: Optional[str]
+    status: ReminderStatus
+    due_at: datetime
+    completed_at: datetime
+    links: List[str]
+    create_time: datetime
